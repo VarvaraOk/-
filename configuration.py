55e4b680224ad41c7a9daf4d72c304b138a64dea
VarvaraOk <okonechnikovav@yandex.ru>
2024-06-16 19:30:42 +0300

    configuration.py

---
+++ configuration.py
@@ -0,0 +1,11 @@
+# URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API или другим ресурсам.
+URL_SERVICE = "https://a255cba2-8c6f-4905-9121-879ad1776866.serverhub.praktikum-services.ru"
+
+# DOC_PATH содержит путь к документации веб-сервиса.
+DOC_PATH = "/docs/"
+
+# NEW_ORDER хранит путь к API-методу для создания нового заказа.
+NEW_ORDER = "/api/v1/orders"
+
+# TRACK_ORDER хранит путь к API-методу для получения заказа по его номеру
+TRACK_ORDER = "/api/v1/orders/track"
